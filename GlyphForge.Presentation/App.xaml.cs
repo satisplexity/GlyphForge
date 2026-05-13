cb2f8547e6c0e6d2a894bcb4971ec6a879fd59f5
satisplexity <satisplexity@gmail.com>
2026-05-13 21:35:55 +0200

    feat: add GlyphForge.Presentation project

---
+++ GlyphForge.Presentation/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace GlyphForge.Presentation
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ GlyphForge.Presentation/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace GlyphForge.Presentation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
