7b82f0af8f7310ba00821bb00b3d764571616c4c
satisplexity <satisplexity@gmail.com>
2026-05-13 22:45:25 +0200

    feat(presentation): add folder selecting

--- GlyphForge.Presentation/MainWindow.xaml.cs
+++ GlyphForge.Presentation/MainWindow.xaml.cs
@@ -1,12 +1,44 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
 
 namespace GlyphForge.Presentation
 {
     public partial class MainWindow : Window
     {
+        private string _selectedFolder;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog dialog = new OpenFolderDialog();
+
+            dialog.Title = "Select folder";
+            
+            if(dialog.ShowDialog() == true)
+            {
+                _selectedFolder = dialog.FolderName;
+
+                _folderName_TextBlock.Text = _selectedFolder;
+            }
+        }
+
+        private void CreateDictionaryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
